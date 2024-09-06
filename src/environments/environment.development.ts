dfc2bd240d44934f613d5c4a1e8114aa388b3c1f
QuiqueVladimir <U202022365@upc.edu.pe>
2024-09-05 20:40:43 -0500

    chore: configured environment variables

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  newsApiKey: 'd759c31290e94f76b6995ce1a5be71bd'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  newsApiKey: 'd759c31290e94f76b6995ce1a5be71bd'
+};
