9f44ceb1b78ff6a31d0cbc95623fabe5470ccb52
QuiqueVladimir <U202022365@upc.edu.pe>
2024-09-05 20:55:51 -0500

    feat(news): implemented news api service client

---
+++ src/app/news/services/news-api.service.ts
@@ -0,0 +1,23 @@
+import { inject, Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsApiService {
+  private apiKey: string = `${environment.newsApiKey}`;
+  private baseUrl: string = 'https://newsapi.org/v2/';
+  private http: HttpClient=inject(HttpClient);
+
+  getSources() {
+    return this.http.get(`${this.baseUrl}/top-headlines/sources?apikey=${this.apiKey}`);
+  }
+
+  getArticlesBySourceId(sourceId: string){
+    return this.http.get(`${this.baseUrl}/top-headlines?sources=${sourceId}&apiKey=${this.apiKey}`);
+  }
+  initArticles(){
+    return this.getArticlesBySourceId('bbc-news');
+  }
+}
